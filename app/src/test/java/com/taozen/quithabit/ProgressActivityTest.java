4b766e4feae1ad43db829162bc3b9f56c3af8fd0
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2019-02-28 17:35:28 +0200

    add test mockup

---
+++ app/src/test/java/com/taozen/quithabit/ProgressActivityTest.java
@@ -0,0 +1,15 @@
+package com.taozen.quithabit;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProgressActivityTest {
+
+    @Test
+    public void onCreate() {
+        Log.d("t", "d");
+    }
+}
